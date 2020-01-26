b5c97e7134202e7ff230515e54e8473813ccd15f
fadimata tillman <fadi.tillman@gmail.com>
2020-01-26 01:02:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
 if phrase == 'I LOVE YOU GRANDMA!'
     "I LOVE YOU TOO PUMPKIN!"
- elsif phrase == "Hi Nana, how are you? "
+ elsif phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
